85c14ce25d60aa47da47c471ed93750b5b513ecd
Carlos Henrique Lopes Zansavio <carloshlzansavio@gmail.com>
2024-04-27 18:45:58 -0300

    Update mapping to the appropriate file and add make test command

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -41,8 +41,5 @@ end)
 -- clipboard
 vim.api.nvim_set_option("clipboard","unnamed")
 
--- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+-- Relative line numbers
+vim.opt.relativenumber = true
--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,10 +1,20 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 --map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- Vertical split screen
+vim.keymap.set('n', '<leader>v', ':vsplit<CR>', {desc = 'Open the file in a vertical split screen'})
+
+-- Make test
+vim.keymap.set('n', '<leader>ta', ':make test<CR>', {desc = 'make test'})
